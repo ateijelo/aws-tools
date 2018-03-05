59bc8d22d3da13d9210be030ac8a5be6942a83bb
Andy Teijelo Pérez <andy.teijelo@alayacare.com>
2018-03-05 11:44:49 -0500

    Don't user a tarball for new-lambda; add .vscode to .gitignore

--- new-lambda/new-lambda.sh
+++ new-lambda/new-lambda.sh
@@ -2,12 +2,11 @@
 
 BINPATH=$(realpath "$0")
 BINDIR=$(dirname "$BINPATH")
-TEMPLATE="$BINDIR/new-lambda.tar.gz"
+TEMPLATE="$BINDIR/new-lambda"
 
 DIR="$1"
 LAMBDA_NAME=$(basename "$DIR")
 
-mkdir -p "$DIR"
-tar -C "$DIR" -xf "$TEMPLATE"
+cp -r "$TEMPLATE" "$DIR"
 
 sed -i "s/__LAMBDA_NAME__/$LAMBDA_NAME/" "$DIR/Makefile"
